fd175aa76342dc3cd4148cc4a3bef2bc88b56f95
Promit-revar <61512425+Promit-revar@users.noreply.github.com>
2022-09-30 12:00:34 +0530

    made it better

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ app.post('/save',async (req,res)=>{
     var code=BuildLink(quiz);
     //console.log(code);
     //console.log()
-    fs.writeFileSync(`./views/partials/${data['qname']}.ejs`,code,err=>{
+    fs.writeFileSync(__dirname+`/views/partials/${data['qname']}.ejs`,code,err=>{
         if(err){
             console.log(err);
         }
